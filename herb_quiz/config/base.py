f8b5ebf9b41711c9f8fbaf157bc01e2c8cf9b7c7
NicolCaresse <nicolchawkins@gmail.com>
2019-12-14 11:42:57 -0800

    Static root

--- herb_quiz/config/base.py
+++ herb_quiz/config/base.py
@@ -113,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 
 # We also needed to add this to specify where the downloads go
