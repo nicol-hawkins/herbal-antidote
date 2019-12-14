b1629edd99b775c40b8516fafc46f8e6d8462d2e
NicolCaresse <nicolchawkins@gmail.com>
2019-12-14 12:47:38 -0800

    adding back collectstatic

--- herb_quiz/config/base.py
+++ herb_quiz/config/base.py
@@ -41,6 +41,7 @@ DJANGO_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
 ]
 
 INSTALLED_APPS = LOCAL_APPS + THIRD_PARTY_APPS + DJANGO_APPS
