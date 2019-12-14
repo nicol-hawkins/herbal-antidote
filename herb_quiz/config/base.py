98f646c3d4a986250ae4186372ee31a6894d6238
NicolCaresse <nicolchawkins@gmail.com>
2019-12-14 11:44:35 -0800

    Static root retry

--- herb_quiz/config/base.py
+++ herb_quiz/config/base.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # We also needed to add this to specify where the downloads go
